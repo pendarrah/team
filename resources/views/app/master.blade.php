7a33d8015581ecdfb8ee0868d1f2d1cbb315a1f7
Mohammad ali moshaee <power2craft@gmail.com>
2020-09-24 22:06:21 +0330

    change title

--- resources/views/app/master.blade.php
+++ resources/views/app/master.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>teamofit</title>
+    <title>teamofit masha</title>
 
     <meta name="keywords" content="" />
     <meta name="description" content="">
